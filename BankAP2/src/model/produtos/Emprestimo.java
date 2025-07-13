495073339261c8a55fe30f75fc690109e6514dc7
Pedro <Pedro@DESKTOP-S780SV0>
2025-07-13 19:26:40 -0300

    pacote modelo.produtos

--- BankAP2/src/model/produtos/Emprestimo.java
+++ BankAP2/src/model/produtos/Emprestimo.java
@@ -4,12 +4,51 @@
  */
 package model.produtos;
 
-import model.produtos.ProdutoBancario;
-
+import model.Cliente;
 /**
  *
  * @author Victor
  */
 public class Emprestimo implements ProdutoBancario {
+    private double valor;
+    private double juros;
+    private int parcelas;
+    private Cliente cliente;
     
+    public Emprestimo(double valor, double juros, int parcelas, Cliente cliente){
+        this.valor = valor;
+        this.juros = juros;
+        this.parcelas = parcelas;
+        this.cliente = cliente;
+    }
+
+    public double calcularValorFinal() {
+        return valor * Math.pow(1 + juros, parcelas);
+    }
+
+    public String getDescricao() {
+        return "Empréstimo de " + valor + " em " + parcelas + " parcelas";
+    }
+
+    //getters
+    public double getValor(){
+        return valor;
+    }
+    public double getJuros(){
+        return juros;
+    }
+    public int getParcelas(){
+        return parcelas;
+    }
+    public Cliente getCliente(){
+        return cliente;
+    }
+    
+    //setters
+    public void setJuros(double juros){
+        this.juros = juros;
+    }
+    public void setParcelas(int parcelas){
+        this.parcelas = parcelas;
+    }
 }
