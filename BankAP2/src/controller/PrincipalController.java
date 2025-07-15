052eebd6758ae138f425783eb65c503965541f00
Willian Rullian <niltomwr@gmail.com>
2025-07-15 17:45:25 -0300

    tela principal

--- BankAP2/src/controller/PrincipalController.java
+++ BankAP2/src/controller/PrincipalController.java
@@ -1,26 +1,76 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
  *
- * @author Victor
+ * @author 
  */
 public class PrincipalController implements Initializable {
+
+    @FXML
+    private Button buttonClientesPrincipal;
+
+    @FXML
+    private Button buttonContasPrincipal;
+
+    @FXML
+    private Button buttonProdutosPrincipal;
+
+    @FXML
+    private TableView<?> clienteTablePrincipal;
+
+    @FXML
+    private TableColumn<?, ?> clientesColumnPrincipal;
+
+    @FXML
+    private TableColumn<?, ?> contaColumnprincipal;
+
+    @FXML
+    private TableView<?> contaTablePrincipal;
+
+    @FXML
+    private TableColumn<?, ?> cpfClienteColumnPrincipal;
+
+    @FXML
+    private TableColumn<?, ?> emailColumnContaPrincipal;
+
+    @FXML
+    private TableColumn<?, ?> idClienteColumnPrincipal;
+
+    @FXML
+    private TableColumn<?, ?> idColumnContarincipal;
+
+    @FXML
+    private TableColumn<?, ?> idColumnProdutosPrincipal;
+
+    @FXML
+    private TableColumn<?, ?> precoColumnProdutoPrincipal;
+
+    @FXML
+    private BorderPane principalBorderPane;
+
+    @FXML
+    private TableView<?> produtoTablePrincipal;
+
+    @FXML
+    private TableColumn<?, ?> produtosColumnPrincipal;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        // TODO: Add initialization logic here
+    }
+
 }
+```
