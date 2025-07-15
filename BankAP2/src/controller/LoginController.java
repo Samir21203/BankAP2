ac49c1d37ddecf4e58752c247ae88a219959d6bc
Willian Rullian <niltomwr@gmail.com>
2025-07-15 17:27:11 -0300

    skeleton login

--- BankAP2/src/controller/LoginController.java
+++ BankAP2/src/controller/LoginController.java
@@ -1,30 +1,31 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ * FXML Controller class
+ *
+ * 
  */
 package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Victor
- */
 public class LoginController implements Initializable {
-@FXML
+
+   @FXML
     private Button buttonLogin;
 
     @FXML
     private Label invalidLloginLabel;
+
+    @FXML
+    private Label labelForgotPassword;
 
     @FXML
     private AnchorPane leftLoginPane;
@@ -33,23 +34,20 @@ public class LoginController implements Initializable {
     private BorderPane loginPane;
 
     @FXML
-    private PasswordField passwordField;
+    private PasswordField passwordFieldLogin;
 
     @FXML
     private AnchorPane rightLoginPane;
 
     @FXML
-    private TextField userField;
+    private TextField userFieldLogin;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-     
+    }
+
 }
-
-
-
-   
